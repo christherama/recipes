22af7232871c36d93c465634531b407d2cb52365
chrisramey <cjramey@gmail.com>
2012-11-16 11:19:50 -0600

    Added layout divs to index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- FAVICON -->
+		<link rel="icon" type="image/png" href="//placehold.it/24x24/FF601C/FFFFFF&text=R" />
+	
+		<!-- STYLESHEETS -->
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		
+		<!-- JAVASCRIPT -->
+		
+		<title>Recipes</title>
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<?php include('layout/header.php') ?>
+			</header>
+			<nav>
+				<?php include('layout/nav.php') ?>
+			</nav>
+			<div id="content">
+				<?php include('layout/content.php') ?>
+			</div>
+			<footer>
+				<?php include('layout/footer.php') ?>
+			</footer>
+		</div>
+	</body>
+</html>
